3764e458ef3a05ed00be51e4ac0fb375e92f9310
Braydon Davis <braydon.davis@mtchs.org>
2017-02-17 08:59:22 -0700

    Project Page Updates

--- Assets/JS/Projects.js
+++ Assets/JS/Projects.js
@@ -3,10 +3,7 @@ $(document).ready(function () {
     $(".projectCard").click(function () {
         var url = $(this).attr("data-content");
         if (url && typeof url == "string") {
-
-            $("body").append("<a href='" + url + "' id='tempClickLink' target=_blank></a>");
-            $("#tempClickLink").click().remove();
-
+			window.open(url);
         }
     });
 
